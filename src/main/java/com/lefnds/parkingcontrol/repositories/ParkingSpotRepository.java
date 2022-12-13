6177934d707fabc73e253e99770dbd5a5bfcde48
Edu <luizeduardofnds@gmail.com>
2022-12-12 22:47:00 -0300

    Project struct

---
+++ src/main/java/com/lefnds/parkingcontrol/repositories/ParkingSpotRepository.java
@@ -0,0 +1,11 @@
+package com.lefnds.parkingcontrol.repositories;
+
+import com.lefnds.parkingcontrol.models.ParkingSpotModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ParkingSpotRepository extends JpaRepository<ParkingSpotModel, UUID> {
+}
---
+++ src/main/java/com/lefnds/parkingcontrol/services/ParkingSpotService.java
@@ -0,0 +1,39 @@
+package com.lefnds.parkingcontrol.services;
+
+import com.lefnds.parkingcontrol.models.ParkingSpotModel;
+import com.lefnds.parkingcontrol.repositories.ParkingSpotRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class ParkingSpotService {
+
+    @Autowired
+    private ParkingSpotRepository repository;
+
+    public List<ParkingSpotModel> findAll() {
+        return repository.findAll();
+    }
+
+    public ParkingSpotModel findById(UUID id) {
+        return repository.findById(id).get();
+    }
+
+    public ParkingSpotModel save(UUID id) {
+        ParkingSpotModel entity = repository.findById(id).get();
+        return repository.save(entity);
+    }
+
+    public ParkingSpotModel update(UUID id, ParkingSpotModel p) {
+        ParkingSpotModel entity = repository.findById(id).get();
+        return repository.save(entity);
+    }
+
+    public void delete(UUID id) {
+        ParkingSpotModel entity = repository.findById(id).get();
+        repository.delete(entity);
+    }
+}
