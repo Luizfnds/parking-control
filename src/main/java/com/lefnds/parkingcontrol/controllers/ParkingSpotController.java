372d48e0ed4e082ef394dad998bd2bdd72714059
Edu <luizeduardofnds@gmail.com>
2022-12-13 17:43:22 -0300

    getAllParkingSpots pagination

--- src/main/java/com/lefnds/parkingcontrol/controllers/ParkingSpotController.java
+++ src/main/java/com/lefnds/parkingcontrol/controllers/ParkingSpotController.java
@@ -6,6 +6,10 @@ import com.lefnds.parkingcontrol.services.ParkingSpotService;
 import jakarta.validation.Valid;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -42,8 +46,8 @@ public class ParkingSpotController {
     }
 
     @GetMapping
-    public ResponseEntity<List<ParkingSpotModel>> getALlParkingSpots() {
-        return ResponseEntity.status(HttpStatus.OK).body(service.findAll());
+    public ResponseEntity<Page<ParkingSpotModel>> getALlParkingSpots(@PageableDefault(page = 0, size = 10,sort = "id", direction = Sort.Direction.ASC) Pageable pageable) {
+        return ResponseEntity.status(HttpStatus.OK).body(service.findAll(pageable));
     }
 
     @GetMapping("/{id}")
--- src/main/java/com/lefnds/parkingcontrol/services/ParkingSpotService.java
+++ src/main/java/com/lefnds/parkingcontrol/services/ParkingSpotService.java
@@ -4,6 +4,8 @@ import com.lefnds.parkingcontrol.models.ParkingSpotModel;
 import com.lefnds.parkingcontrol.repositories.ParkingSpotRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,8 +18,8 @@ public class ParkingSpotService {
     @Autowired
     private ParkingSpotRepository repository;
 
-    public List<ParkingSpotModel> findAll() {
-        return repository.findAll();
+    public Page<ParkingSpotModel> findAll(Pageable pageable) {
+        return repository.findAll(pageable);
     }
 
     public Optional<ParkingSpotModel> findById(UUID id) {
