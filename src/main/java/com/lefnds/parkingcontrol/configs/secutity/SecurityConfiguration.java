efabe45dad1ad69d5e321c29771d01b1ea0b95fd
Edu <luizeduardofnds@gmail.com>
2022-12-14 18:21:03 -0300

    Password encode.

--- src/main/java/com/lefnds/parkingcontrol/configs/secutity/SecurityConfiguration.java
+++ src/main/java/com/lefnds/parkingcontrol/configs/secutity/SecurityConfiguration.java
@@ -1,8 +1,14 @@
 package com.lefnds.parkingcontrol.configs.secutity;
 
+import org.apache.catalina.UserDatabase;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,6 +23,21 @@ public class SecurityConfiguration {
                 .and()
                 .csrf().disable();
         return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+        UserDetails userDetails = User.builder()
+                .username("LEDU")
+                .password(passwordEncoder().encode("1234"))
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(userDetails);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
 
