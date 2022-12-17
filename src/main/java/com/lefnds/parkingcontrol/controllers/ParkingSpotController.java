7752c69a604f841b75db91b73fd87e0aecfff566
Edu <luizeduardofnds@gmail.com>
2022-12-17 13:51:56 -0300

    Configuring security without WebSecurityConfigurerAdapter and authorization by method.

--- src/main/java/com/lefnds/parkingcontrol/controllers/ParkingSpotController.java
+++ src/main/java/com/lefnds/parkingcontrol/controllers/ParkingSpotController.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -28,6 +29,7 @@ public class ParkingSpotController {
     @Autowired
     private ParkingSpotService service;
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PostMapping
     public ResponseEntity<Object> saveParkingSpot(@RequestBody @Valid ParkingSpotDto parkingSpotDto) {
         if(service.existsByLicensePlateCar(parkingSpotDto.getLicensePlateCar())){
@@ -45,11 +47,13 @@ public class ParkingSpotController {
         return ResponseEntity.status(HttpStatus.CREATED).body(service.save(parkingSpotModel));
     }
 
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_USER')")
     @GetMapping
     public ResponseEntity<Page<ParkingSpotModel>> getALlParkingSpots(@PageableDefault(page = 0, size = 10,sort = "id", direction = Sort.Direction.ASC) Pageable pageable) {
         return ResponseEntity.status(HttpStatus.OK).body(service.findAll(pageable));
     }
 
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_USER')")
     @GetMapping("/{id}")
     public ResponseEntity<Object> getOneParkingSpot(@PathVariable(value = "id") UUID id) {
         Optional<ParkingSpotModel> entity = service.findById(id);
@@ -59,6 +63,7 @@ public class ParkingSpotController {
         return ResponseEntity.status(HttpStatus.OK).body(entity.get());
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @DeleteMapping("/{id}")
     public ResponseEntity<Object> deleteParkingSpot(@PathVariable UUID id) {
         Optional<ParkingSpotModel> entity = service.findById(id);
@@ -69,6 +74,7 @@ public class ParkingSpotController {
         return ResponseEntity.status(HttpStatus.OK).body("Parking Spot deleted successfully");
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PutMapping("/{id}")
     public ResponseEntity<Object> updateParkingSpot(@PathVariable UUID id,
                                                     @RequestBody ParkingSpotDto entityDto) {
